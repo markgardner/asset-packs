153d899dccf07ac4c9e534f093bd9d5ed59aecb8
Mark Gardner <mark.gardner@progfinance.com>
2014-02-07 13:54:50 -0700

    Adding hinting and fixing closeAll bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,58 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'lib/**/*.js',
-        'tasks/*.js'
-      ],
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true
-      }
-    },
+	grunt.initConfig({
+	jshint: {
+		all: [
+			'lib/**/*.js',
+			'middleware/**/*.js',
+			'tasks/*.js',
+			'test/**/*.js',
+			'Gruntfile.js'
+		],
+		options: {
+			curly: true,
+			eqeqeq: true,
+			immed: true,
+			latedef: true,
+			newcap: true,
+			noarg: true,
+			sub: true,
+			undef: true,
+			boss: true,
+			eqnull: true,
+			node: true,
+			globals: {
+				describe: false,
+				it: false,
+				before: false,
+				beforeEach: false,
+				after: false,
+				afterEach: false
+			}
+		}
+	},
 
-    // Unit tests.
-    mochaTest: {
-      test: {
-        src: ['test/*_test.js']
-      }
-    },
+	// Unit tests.
+	mochaTest: {
+		test: {
+			src: ['test/*_test.js']
+		}
+	},
 
-    watch: {
-      tests: {
-        files: ['test/*_test.js','lib/**/*'],
-        tasks: ['test']
-      }
-    }
-  });
+	watch: {
+		tests: {
+			files: ['test/*_test.js','lib/**/*'],
+			tasks: ['test']
+		}
+	}
+	});
 
-  grunt.loadTasks('tasks');
+	grunt.loadTasks('tasks');
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-mocha-test');
 
-  grunt.registerTask('test', ['jshint', 'mochaTest']);
-  grunt.registerTask('default', ['test']);
+	grunt.registerTask('test', ['jshint', 'mochaTest']);
+	grunt.registerTask('default', ['test']);
 };
