d94ff7e5584695f8a90c799f05e9d97221324326
Mark Gardner <mark.gardner@progfinance.com>
2014-02-07 13:42:06 -0700

    Adding grunt plugin

--- tasks/asset_packs.js
+++ tasks/asset_packs.js
@@ -1,35 +1,39 @@
 'use strict';
 
 var path = require('path'),
+    async = require('async'),
+    fs = require('node-fs'),
     AssetPacker = require('../lib/AssetPacker');
 
 module.exports = function(grunt) {
-  grunt.registerMultiTask(module.exports.task, module.exports.desc, module.exports.handle(grunt));
-};
+    grunt.registerMultiTask('packs', 'Grunt Plugin for writing packs to disk', function() {
+        var outputDir = path.resolve(this.data.dest),
+            done = this.async(),
+            packs = [];
 
-module.exports.task = 'asset_packs';
-module.exports.desc = 'Grunt Plugin for packing development source into production assets';
-module.exports.handle = function(grunt) {
-  return function() {
-    var packFiles = [],
-        options = this.options(),
-        done = this.async(),
-        packer;
+        for(var list = this.filesSrc, i = 0; i < list.length; i++) {
+            packs.push(new AssetPacker({
+                pack: path.resolve(list[i]),
+                base: path.dirname(list[i]),
+                autoWatch: false
+            }));
+        }
 
-    this.files.forEach(function(file) {
-      packFiles = packFiles.concat(file.src);
+        async.each(packs, function(pack, done) {
+            pack.getAllContent(function(content) {
+                for(var p in content) {
+                    var filename = path.resolve(outputDir, pack.compiler.rootDir + '.' + p);
+                    var dirname = path.dirname(filename);
+
+                    if(!fs.existsSync(dirname)) {
+                        fs.mkdirSync(dirname, 511 /* 0777 */, true);
+                    }
+
+                    fs.writeFile(filename, content[p].content);
+                }
+
+                done();
+            });
+        }, done);
     });
-
-    packFiles = packFiles.map(function(file) {
-      return path.resolve(file);
-    });
-
-    options.dest = path.resolve(options.dest);
-    options.packFiles = packFiles;
-
-    packer = new AssetPacker(options);
-    packer.compile(done);
-
-    return packer;
-  };
 };
