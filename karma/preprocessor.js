670e213192b5553190004997032ebb49fd77e295
Mark Gardner <mark.gardner@progfinance.com>
2014-04-06 15:20:49 -0600

    Adding better support for updating karma files on pack file change

--- karma/preprocessor.js
+++ karma/preprocessor.js
@@ -1,9 +1,10 @@
 var path = require('path'),
+	fs = require('fs'),
 	AssetPacker = require('../lib/AssetPacker');
 
 var assets = {};
 
-function createPreprocessor(basePath) {
+function createPreprocessor(basePath, singleRun) {
 	return function(content, file, done) {
 		var packName = path.relative(basePath, file.path);
 		packName = path.dirname(packName);
@@ -11,7 +12,13 @@ function createPreprocessor(basePath) {
 		if(!assets[packName]) {
 			assets[packName] = new AssetPacker({
 				pack: file.path,
-				base: path.dirname(file.path)
+				base: path.dirname(file.path),
+				autoWatch: !singleRun
+			});
+
+			assets[packName].on('fileChange', function() {
+				var now = Date.now();
+				fs.utimesSync(file.path, now, now);
 			});
 		}
 
@@ -21,6 +28,6 @@ function createPreprocessor(basePath) {
 	};
 }
 
-createPreprocessor.$inject = ['config.basePath'];
+createPreprocessor.$inject = ['config.basePath', 'config.singleRun'];
 
 module.exports = createPreprocessor;
